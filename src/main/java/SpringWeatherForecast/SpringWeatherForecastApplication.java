730f49434abe72c5a28387d12023a2065f152d18
x3matt <x3matt21@mail.ru>
2019-09-29 02:13:42 +0700

    Initial commit

---
+++ src/main/java/SpringWeatherForecast/SpringWeatherForecastApplication.java
@@ -0,0 +1,13 @@
+package SpringWeatherForecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeatherForecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeatherForecastApplication.class, args);
+	}
+
+}
